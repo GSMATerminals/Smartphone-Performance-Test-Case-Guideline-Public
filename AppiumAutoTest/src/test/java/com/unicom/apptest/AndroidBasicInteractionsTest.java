6e67f2fbdebae505e49d906b678712691abfd070
woodsjiang <woodsjiang@gmail.com>
2020-12-01 10:25:41 +0800

    Open the "Alert Dialog" activity of the android app

--- AppiumAutoTest/src/test/java/com/unicom/apptest/AndroidBasicInteractionsTest.java
+++ AppiumAutoTest/src/test/java/com/unicom/apptest/AndroidBasicInteractionsTest.java
@@ -50,4 +50,22 @@ public class AndroidBasicInteractionsTest extends BaseTest {
         Assert.assertEquals(searchTextValue, "Hello world!");
     }
 
+    @Test
+    public void testOpensAlert() {
+        // Open the "Alert Dialog" activity of the android app
+        driver.startActivity(new Activity(PACKAGE, ALERT_DIALOG_ACTIVITY));
+
+        // Click button that opens a dialog
+        AndroidElement openDialogButton = (AndroidElement) driver.findElementById("io.appium.android.apis:id/two_buttons");
+        openDialogButton.click();
+
+        // Check that the dialog is there
+        AndroidElement alertElement = (AndroidElement) driver.findElementById("android:id/alertTitle");
+        String alertText = alertElement.getText();
+        Assert.assertEquals(alertText, "Lorem ipsum dolor sit aie consectetur adipiscing\nPlloaso mako nuto siwuf cakso dodtos anr koop.");
+        AndroidElement closeDialogButton = (AndroidElement) driver.findElementById("android:id/button1");
+
+        // Close the dialog
+        closeDialogButton.click();
+    }
 }
