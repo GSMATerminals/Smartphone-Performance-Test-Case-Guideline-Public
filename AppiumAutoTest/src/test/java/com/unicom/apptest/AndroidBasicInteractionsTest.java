2bc839f63813581fd099285601c47a141137b800
woodsjiang <woodsjiang@gmail.com>
2020-12-01 09:59:43 +0800

    Add android basic interactions test class

---
+++ AppiumAutoTest/src/test/java/com/unicom/apptest/AndroidBasicInteractionsTest.java
@@ -0,0 +1,40 @@
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AndroidBasicInteractionsTest extends BaseTest {
+    private AndroidDriver<WebElement> driver;
+    private final String SEARCH_ACTIVITY = ".app.SearchInvoke";
+    private final String ALERT_DIALOG_ACTIVITY = ".app.AlertDialogSamples";
+    private final String PACKAGE = "io.appium.android.apis";
+
+    @BeforeClass
+    public void setUp() throws IOException {
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "../apps");
+        File app = new File(appDir.getCanonicalPath(), "ApiDemos-debug.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new AndroidDriver<WebElement>(getServiceUrl(), capabilities);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
