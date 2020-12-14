169b273904670eaf24154f3b5fd44c4f7e889f9c
woodsjiang <woodsjiang@gmail.com>
2020-12-14 10:42:02 +0800

    basic iOS interactions test

---
+++ AppiumAutoTest/src/test/javascript/apptest/ios-basic-interactions.test.js
@@ -0,0 +1,51 @@
+import wd from 'wd';
+import chai from 'chai';
+import {
+  iosCaps, serverConfig, iosTestApp, SAUCE_TESTING, SAUCE_USERNAME,
+  SAUCE_ACCESS_KEY
+} from '../helpers/config';
+
+const {assert} = chai;
+
+describe('Basic IOS interactions', function () {
+  let driver;
+  let allPassed = true;
+
+  before(async function () {
+    // Connect to Appium server
+    driver = SAUCE_TESTING
+      ? await wd.promiseChainRemote(serverConfig)
+      : await wd.promiseChainRemote(serverConfig, SAUCE_USERNAME, SAUCE_ACCESS_KEY);
+
+    // add the name to the desired capabilities
+    const sauceCaps = SAUCE_TESTING
+      ? {
+        name: 'iOS Basic Interactions Test',
+      }
+      : {};
+
+    // merge all the capabilities
+    const caps = {
+      ...iosCaps,
+      ...sauceCaps,
+      app: iosTestApp,
+    };
+
+    // Start the session, merging all the caps
+    await driver.init(caps);
+  });
+
+  afterEach(function () {
+    // keep track of whether all the tests have passed, since mocha does not do this
+    allPassed = allPassed && (this.currentTest.state === 'passed');
+  });
+
+  after(async function () {
+    await driver.quit();
+    if (SAUCE_TESTING && driver) {
+      await driver.sauceJobStatus(allPassed);
+    }
+  });
+
+
+});
